abc2396d3419df8d809cadb5ebadbddce2691baf
KoreneS <korene17@gmail.com>
2020-03-20 21:57:03 -0400

    Update 32020

--- apap/app/src/main/java/com/example/apap/AddCardActivity.java
+++ apap/app/src/main/java/com/example/apap/AddCardActivity.java
@@ -30,19 +30,17 @@ public class AddCardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-            String newQuestion = ((EditText) findViewById(R.id.questionInput)).getText().toString(); // create a new Intent, this is where we will put our data
-                String newAnswer = ((EditText) findViewById(R.id.questionAnswer)).getText().toString();
-                Intent data = new Intent();
-            data.putExtra("string1", newQuestion); // puts one string into the Intent, with the key as 'string1'
-            data.putExtra("string2", newAnswer); // puts another string into the Intent, with the key as 'string2
-            setResult(RESULT_OK, data); // set result code and bundle data for response
-            finish();
-    }
-});
+                //if (flashcard_question) {
 
-
-
-
-
+                    String newQuestion = ((EditText) findViewById(R.id.questionInput)).getText().toString(); // create a new Intent, this is where we will put our data
+                    String newAnswer = ((EditText) findViewById(R.id.questionAnswer)).getText().toString();
+                    Intent data = new Intent();
+                    data.putExtra("string1", newQuestion); // puts one string into the Intent, with the key as 'string1'
+                    data.putExtra("string2", newAnswer); // puts another string into the Intent, with the key as 'string2
+                    setResult(RESULT_OK, data); // set result code and bundle data for response
+                    finish();
+                //}
+            }
+        });
     }
 }
--- apap/app/src/main/java/com/example/apap/MainActivity.java
+++ apap/app/src/main/java/com/example/apap/MainActivity.java
@@ -1,27 +1,26 @@
 package com.example.apap;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
 import static android.view.View.INVISIBLE;
-import static com.example.apap.R.id.flashcard_answer;
 import static com.example.apap.R.id.flashcard_question;
-import static com.example.apap.R.id.nameOfView;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
+    int currentCardDisplayedIndex = 0;
+    List<Flashcard> allFlashcards;
+    FlashcardDatabase flashcardDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
         findViewById(flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(flashcard_question).setVisibility(INVISIBLE);
             }
         });
-
         findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,18 +35,41 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent, 100);
             }
         });
-
-
-
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
+        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            }
+        });
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String string1;
+        String string2;
         if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
-
-            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
-            String string2 = data.getExtras().getString("string2");
-            ((TextView)findViewById(R.id.flashcard_question)).setText(string1);
-            ((TextView)findViewById(R.id.flashcard_answer)).setText(string2);}
+            string1 = data.getExtras().getString("string1");
+            string2 = data.getExtras().getString("string2");
+            ((TextView) findViewById(R.id.flashcard_question)).setText(string1);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(string2);
+            flashcardDatabase.insertCard(new Flashcard(string1, string2));
+            allFlashcards = flashcardDatabase.getAllCards();
+        }
     }
 }
+
+
+
+
+
