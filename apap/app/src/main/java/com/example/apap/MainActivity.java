c1db9e54baa61c557fe5c95c319c7913f3bde95b
KoreneS <korene17@gmail.com>
2020-04-02 22:44:54 -0400

    Test1

--- apap/app/src/main/java/com/example/apap/MainActivity.java
+++ apap/app/src/main/java/com/example/apap/MainActivity.java
@@ -1,31 +1,43 @@
 package com.example.apap;
-
+import android.animation.Animator;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
-
-import static android.view.View.INVISIBLE;
-import static com.example.apap.R.id.flashcard_question;
-
 public class MainActivity extends AppCompatActivity {
     int currentCardDisplayedIndex = 0;
     List<Flashcard> allFlashcards;
     FlashcardDatabase flashcardDatabase;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         flashcardDatabase = new FlashcardDatabase(getApplicationContext());
-        findViewById(flashcard_question).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(flashcard_question).setVisibility(INVISIBLE);
+                View answerSideView = findViewById(R.id.flashcard_answer);
+                View questionSideView = findViewById(R.id.flashcard_question);
+                // get the center for the clipping circle
+                int cx = answerSideView.getWidth() / 2;
+                int cy = answerSideView.getHeight() / 2;
+                // get the final radius for the clipping circle
+                float finalRadius = (float) Math.hypot(cx, cy);
+                // create the animator for this view (the start radius is zero)
+                Animator anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy, 0f, finalRadius);
+                // hide the question and show the answer to prepare for playing the animation!
+                questionSideView.setVisibility(View.INVISIBLE);
+                answerSideView.setVisibility(View.VISIBLE);
+                anim.setDuration(3000);
+                anim.start();
             }
         });
         findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener() {
@@ -33,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
             }
         });
         if (allFlashcards != null && allFlashcards.size() > 0) {
